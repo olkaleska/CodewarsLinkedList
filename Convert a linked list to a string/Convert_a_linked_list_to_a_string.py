a1445263bbe3f3ece0f2522f063d5e26b401e5da
olkaleska <147612442+olkaleska@users.noreply.github.com>
2024-04-05 00:05:19 +0300

    adding working code

--- Convert a linked list to a string/Convert_a_linked_list_to_a_string.py
+++ Convert a linked list to a string/Convert_a_linked_list_to_a_string.py
@@ -1,1 +1,11 @@
-# 
+# Стрінга
+def stringify(node):
+    if isinstance(node, Node):
+        curr_node = node
+        result = str(curr_node.data) + ' -> '
+        while curr_node.next:
+            curr_node = curr_node.next
+            result += str(curr_node.data) + ' -> '
+        result += 'None'
+        return result
+    return 'None'
