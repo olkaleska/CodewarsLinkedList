62f764c9b749985625a4c324f8979d9d148b3ca5
olkaleska <147612442+olkaleska@users.noreply.github.com>
2024-04-08 18:00:32 +0300

    Adding code

--- Parse a linked list from a string/Parse_a_linked_list_from_a_string.py
+++ Parse a linked list from a string/Parse_a_linked_list_from_a_string.py
@@ -1,1 +1,23 @@
 # 
+class Node:
+    def __init__(self, data, next=None): 
+        self.data = data
+        self.next = next
+
+
+def linked_list_from_string(s):
+    splited = s.split(' -> ')[::-1]
+    splited[0] = None
+    for i, el in enumerate(splited):
+        head = None
+        if el == None:
+            pass
+        elif i == 1:
+            node = Node(int(el), None)
+            curr = node
+        elif i == len(splited) - 1:
+            head = Node(int(el), curr)
+        else:
+            node = Node(int(el), curr)
+            curr = node
+    return head
